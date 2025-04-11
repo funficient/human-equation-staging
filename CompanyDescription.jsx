e0551626f2fffe92cd21836126ea8cf4f6ac92ed
karindames <karin.dames@hotmail.com>
2025-04-11 13:49:09 +0200

    add about us page

---
+++ CompanyDescription.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+const CompanyDescription = () => {
+  return (
+    <div className="company-description">
+      The Human Equation is regarded as thought leaders in their field of
+      specialty providing world class consulting &amp; training /development
+      programmes to help our clients reach their inherent, yet often untapped,
+      potential of converting productivity into viable business solutions and
+      solid return-on-investment methodologies.
+    </div>
+  );
+};
+
+export default CompanyDescription;
