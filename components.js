bc15d44aeb87b6fab54e030cfd038b325b5a1c74
karindames <karin.dames@hotmail.com>
2025-04-12 10:54:11 +0200

    Update component head

---
+++ components.js
@@ -0,0 +1,18 @@
+  // Load the header component
+fetch('__header.html')
+.then(response => response.text())
+.then(data => {
+  document.getElementById('header-container').innerHTML = data;
+
+  // Dynamically set the active class after the header is loaded
+  const links = document.querySelectorAll('.nav-link');
+  links.forEach(link => {
+    // Remove the active class from all links
+    link.classList.remove('nav-item-active');
+
+    // Add the active class only to the matching link
+    if (link.href === window.location.href) {
+      link.classList.add('nav-item-active');
+    }
+  });
+});
